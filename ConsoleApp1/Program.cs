ef50ce60eae1603fdbb3c37183918ad94a89a453
BohdanKohanyuk <ipz234_kbb@student.ztu.edu.ua>
2025-02-26 18:08:41 +0200

    Перший коміт

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+                        System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            double x, y, z;
+            bool ok;
+            Console.WriteLine("Лабораторна робота №2.\nВиконав: Іванов І.І., група ПІ-67\nВаріант №17.\nЗавдання 1.");            
+            do
+            {
+                Console.Write("Введіть дробове значення x = ");
+                ok = double.TryParse(Console.ReadLine(), out x);
+                if (!ok)
+                    Console.WriteLine("  Помилка введення значення x. Будь-ласка повторіть введення значення ще раз!");
+            } while (!ok);
+            do
+            {
+                Console.Write("Введіть дробове значення y = ");
+                ok = double.TryParse(Console.ReadLine(), out y);
+                if (!ok)
+                    Console.WriteLine("  Помилка введення значення y. Будь-ласка повторіть введення значення ще раз!");
+            } while (!ok);
+            do
+            {
+                Console.Write("Введіть дробове значення z = ");
+                ok = double.TryParse(Console.ReadLine(), out z);
+                if (!ok)
+                    Console.WriteLine("  Помилка введення значення z. Будь-ласка повторіть введення значення ще раз!");
+            } while (!ok);
+            double r = x + y + z;
+            Console.WriteLine("Результат обчислення: r = {0:F3}", r);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y, z;
+            bool ok;            
+            ok = double.TryParse(textBoxX.Text, out x);
+            if (!ok)
+            {
+                MessageBox.Show("Помилка введення значення x!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            ok = double.TryParse(textBoxY.Text, out y);
+            if (!ok)
+            {
+                MessageBox.Show("Помилка введення значення y!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            ok = double.TryParse(textBoxZ.Text, out z);
+            if (!ok)
+            {
+                MessageBox.Show("Помилка введення значення z!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            double r = x + y + z;
+            textBoxR.Text = r.ToString("F2");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+                        System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+        }
+    }
+}
